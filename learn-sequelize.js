3e514d9a13b2d84bd2c02d74a89e0b80db3f3605
Seth Marcus <seth.marcus07@qmail.cuny.edu>
2021-11-11 19:49:27 -0500

    finished. forgot to clone

--- learn-sequelize.js
+++ learn-sequelize.js
@@ -1,11 +1,11 @@
-const { Genre } = require('./models');
+const { Actor, Genre, Movie } = require('./models');
 
 /*
   currently, the genre table only has 3 entries: Action, Comedy, and Drama
   Add one more Genre of your choice.
 */
 function insertNewGenre() {
-
+  return Genre.create({name: 'col'});
 }
 
 /*
@@ -13,27 +13,41 @@ function insertNewGenre() {
   Add one more Movie of your choice. But it CANNOT be from 2008.
 */
 function insertNewMovie() {
-
+  return Movie.create({title: 'new movie', year:'2022', genreid:1});
 }
 
 /*
   Return the title of the movie with ID=2
 */
 function getMovieWithId2() {
-
+  return Movie.findAll({
+    attributes: ['title'],
+    where:{
+      id: '2'
+    }
+  }).then(movie => movie[0].title);
 }
 
 /*
   Return an array of all the actor names
 */
 function getAllActors() {
-
+  return Actor.findAll({
+    attributes: ['name']
+  }).then(actors => actors.map(a => a.name));
+  
 }
 
 /*
   Return an array of all the movie names from 2008
 */
 function getAllMoviesFrom2008() {
+  return Movie.findAll({
+    attributes: ['title'],
+    where:{
+      year: '2008'
+    }
+  }).then(movies => movies.map(movie => movie.title));
 
 }
 
@@ -41,7 +55,15 @@ function getAllMoviesFrom2008() {
   Delete the genre you added in the first test
 */
 function deleteGenreYouAdded() {
-
+  /*return Genre.destroy({
+    where :{
+      name : 'col'
+    }
+  });*/
+  return Genre.findOne({where: {name: "col"}})
+    .then((g) => {
+      return g.destroy();
+    })
 }
 
 /*
@@ -49,7 +71,14 @@ function deleteGenreYouAdded() {
   Add this association.
 */
 function associateRosarioToEagleEye() {
+  const moviePromise = Movie.findByPk(4);
+  const actorPromise = Actor.findOne({where : {name: 'Rosario Dawson'}});
 
+  return Promise
+    .all( [ moviePromise, actorPromise] )
+    .then(([movieResult, actorResult])=> {
+      return movieResult.addActor(actorResult);
+    });
 }
 
 /*
@@ -57,7 +86,10 @@ function associateRosarioToEagleEye() {
   Add this association.
 */
 function associateRobertToTropicThunder() {
+  const robert = await Actor.findOne({where: {name: "Robert Downey Jr."}});
+  const movie = await Movie.findOne({where: {title : "Tropic Thunder"}});
 
+  return movie.addActor(robert);
 }
 
 module.exports = {
