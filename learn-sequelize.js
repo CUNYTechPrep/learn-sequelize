fa3045344ae16cc42a7a939ba8d92133896a0184
LuisBcode <ab.luis04@gmail.com>
2021-11-11 19:47:56 -0500

    Complete lab

--- learn-sequelize.js
+++ learn-sequelize.js
@@ -1,11 +1,11 @@
-const { Genre } = require('./models');
+const { Genre, Movie, Actor } = require('./models');
 
 /*
   currently, the genre table only has 3 entries: Action, Comedy, and Drama
   Add one more Genre of your choice.
 */
 function insertNewGenre() {
-
+  return Genre.create({ name: 'Horror' });
 }
 
 /*
@@ -13,13 +13,16 @@ function insertNewGenre() {
   Add one more Movie of your choice. But it CANNOT be from 2008.
 */
 function insertNewMovie() {
-
+  return Movie.create({ title: 'Random Movie', year: '2003', genreId: 1 })
 }
 
 /*
   Return the title of the movie with ID=2
 */
 function getMovieWithId2() {
+
+  return Movie.findByPk(2) // .findByPk uses the primary key to find single entry from table
+   .then(movie => {return movie.title}); //can user movie.get('title') too
 
 }
 
@@ -28,20 +31,37 @@ function getMovieWithId2() {
 */
 function getAllActors() {
 
+  return Actor.findAll({attributes: ['name']})
+  .then(actors => {return actors.map(actor => actor.name); 
+  });
+
 }
 
 /*
   Return an array of all the movie names from 2008
 */
 function getAllMoviesFrom2008() {
-
+  return Movie.findAll({ where: {year: 2008}})
+  .then(movies => {return movies.map(movie => movie.title);
+  });
 }
 
 /*
   Delete the genre you added in the first test
 */
 function deleteGenreYouAdded() {
-
+  return Genre.destroy({
+    where: {
+      name: 'Horror'
+    }
+  });
+  // FROM SOLUTION ANOTHER OPTION IS
+//   return Genre
+//     .findOne({where: { name: "SciFi"}})
+//     .then((g) => {
+//       return g.destroy();
+//     })
+// 
 }
 
 /*
@@ -49,7 +69,16 @@ function deleteGenreYouAdded() {
   Add this association.
 */
 function associateRosarioToEagleEye() {
+  let moviePromise = Movie.findOne({where: {title: 'Eagle Eye'}});
+  // if the movie id was know you could use .findByPk(4) with the key of that movie
+  let actorPromise = Actor.findOne({where: {name: 'Rosario Dawson'}});
 
+  return Promise
+  .all([moviePromise, actorPromise])
+  .then(([movieResult, actorResult]) => {
+    return movieResult.addActor(actorResult); // Ask about .addActor() function
+    // return actorResult.addActor(movieResult); // IS ALSO VALIE
+  }) 
 }
 
 /*
@@ -57,8 +86,31 @@ function associateRosarioToEagleEye() {
   Add this association.
 */
 function associateRobertToTropicThunder() {
+  let moviePro = Movie.findOne({where: {title: 'Tropic Thunder'}});
+  let actorPro = Actor.findOne({where: {name: 'Robert Downey Jr.'}});
 
+  return Promise
+  .all([moviePro, actorPro])
+  .then(([movieRes, actorRes]) => {
+    return movieRes.addActor(actorRes);
+  })
 }
+
+/*  ANOTHER WAY FROM SOULTION   */
+// async function associateRobertToTropicThunder() {
+//   // return Actor.findOne({where: {name: "Robert Downey Jr."}})
+//   //   .then(robert => {
+//   //     return Movie.findOne({where: {title: "Tropic Thunder"}})
+//   //       .then(movie => {
+//   //         return movie.addActor(robert);
+//   //       })
+//   //   })
+
+//     let robert = await Actor.findOne({where: {name: "Robert Downey Jr."}});
+//     let movie = await Movie.findOne({where: {title: "Tropic Thunder"}});
+
+//     return movie.addActor(robert);
+// }
 
 module.exports = {
   insertNewGenre,
